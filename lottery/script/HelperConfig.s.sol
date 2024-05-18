c11a0efb8273072a82f068b6b7d76a037bf1b1fd
liangalv <alvin.liang@mail.utoronto.ca>
2024-05-17 20:45:50 -0400

    completed Raffle deploy script

---
+++ lottery/script/HelperConfig.s.sol
@@ -0,0 +1,78 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+
+contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+    struct NetworkConfig {
+        uint256 entranceFee;
+        uint256 interval;
+        address vrfCoordinator;
+        bytes32 gasLane;
+        uint64 subscriptionId;
+        uint32 callbackGasLimit;
+    }
+
+    constructor() {
+        if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30,
+                vrfCoordinator: 0x271682DEB8C4E0901D1a1550aD2e64D568E69909,
+                gasLane: 0x8af398995b04c28e9951adb9721ef74c74f93e6a478f39e7e0777be13527e7ef,
+                subscriptionId: 0,
+                callbackGasLimit: 500000
+            });
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entranceFee: 0.1 ether,
+                interval: 30,
+                vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+                gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+                subscriptionId: 0,
+                callbackGasLimit: 500000
+            });
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.vrfCoordinator != address(0)) {
+            return activeNetworkConfig;
+        }
+        uint96 baseFee = 0.25 ether; // 0.25 LINK
+        uint96 gasPriceLink = 1e9; // 1 gwei LINK
+        vm.startBroadcast();
+        //Two parameters for the mock:
+        //Base fee: gets paid in chainlink tokens
+        //gasPriceLink: how much link gets paid for each additional gas that you use
+        //See when the Chainlink node makes this call it's expending gas, but you have to reimburse it with Link
+        VRFCoordinatorV2Mock vrfCoordinatorMock = new VRFCoordinatorV2Mock(
+            baseFee,
+            gasPriceLink
+        );
+        vm.stopBroadcast();
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30,
+                vrfCoordinator: address(vrfCoordinatorMock),
+                gasLane: 0x0, //gasLane won't matter
+                subscriptionId: 0, // our script will add this
+                callbackGasLimit: 500000
+            });
+    }
+}
---
+++ lottery/src/Raffle.sol
@@ -0,0 +1,169 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+/**
+ * @title A Sample Raffle contract
+ * @author liangalv
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements ChainLink VRFv2
+ */
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
+
+contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
+    error Raffle__NotEnoughEthSent();
+    error Raffle__TransferFailed();
+    error Raffle__RaffleNotOpen();
+    error Raffle__UpkeepNotNeeded(
+        uint256 currentBalance,
+        uint256 numPlayers,
+        uint256 rafffleState
+    );
+    /**Type Declarations */
+
+    //In Solidity, enums can be explicitly converted to integer values
+    enum RaffleState {
+        OPEN, //0
+        CALCULATING //1
+    }
+
+    /**State Variables  */
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint256 private immutable i_entranceFee;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+
+    //@dev Duration of lottery in seconds
+    uint256 private immutable i_interval;
+    //We're going to use a dynamic array because we can loop through and append whereas a mapping can't be looped through
+    //Because we're going to need to eventually pay out these players they gotta be payable
+    address payable[] private s_players;
+    uint256 private s_lastTimestamp;
+    address private s_recentWinner;
+    RaffleState s_raffleState;
+
+    /**Events */
+    event EnteredRaffle(address indexed player);
+    event PickedWinner(address indexed winner);
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
+        i_entranceFee = entranceFee;
+        i_callbackGasLimit = callbackGasLimit;
+        i_interval = interval;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        s_lastTimestamp = block.timestamp;
+        s_raffleState = RaffleState.OPEN;
+    }
+
+    //We want this to be external as it's both more gas efficient and prevents internal calls
+    function enterRaffle() external payable {
+        if (msg.value != i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+        }
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__RaffleNotOpen();
+        }
+        s_players.push(payable(msg.sender));
+        //As rule of thumb everytime a storage var state changes we should emit an event
+        //1. Makes contract migration (update) easier
+        //2. Makes front end "indexing" easier
+        emit EnteredRaffle(msg.sender);
+    }
+
+    /**
+     * @dev This is a function that the chainlink automation nodes call in
+     * order to see if it's time to perform an upkeep
+     * The following should be true for this to return true
+     * 1. The interval has passed between raffle runs
+     * 2. The raffle is in the OPEN state
+     * 3. The contract has ETH (aka Players)
+     * 4. (Implicit) The subscription is funded with LINK
+     */
+    function checkUpkeep(
+        bytes memory /*checkData*/
+    )
+        public
+        view
+        override
+        returns (bool upkeepNeeded, bytes memory /*performData*/)
+    {
+        bool enoughTimePassed = (block.timestamp - s_lastTimestamp) >
+            i_interval;
+        bool raffleOpen = s_raffleState == RaffleState.OPEN;
+        bool hasBalance = address(this).balance > 0;
+        bool hasPlayers = s_players.length > 0;
+        upkeepNeeded =
+            enoughTimePassed &&
+            raffleOpen &&
+            hasBalance &&
+            hasPlayers;
+        //Better to be explicit to return upkeepNeeded and a blank bytes object even if not required
+        return (upkeepNeeded, bytes(""));
+    }
+
+    /**
+     * 1. Get a random number
+     * 2. Use the random number to pick a playe
+     * 3. Be automatically called
+     */
+    //calldata refers to a memory location specific to external function calls, which
+    //cannot be modified
+    function performUpkeep(bytes calldata /* performData */) external {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Raffle__UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_raffleState)
+            );
+        }
+        if (block.timestamp - s_lastTimestamp <= i_interval) {
+            revert();
+        }
+        s_raffleState = RaffleState.CALCULATING;
+        i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256 /*requestId*/,
+        uint256[] memory randomWords
+    ) internal override {
+        //Modulo gives us the remainder value which is always in the range of 0 -> len(s_players)
+        uint256 winnerIndex = randomWords[0] % s_players.length;
+        address payable winner = s_players[winnerIndex];
+        s_recentWinner = winner;
+        s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimestamp = block.timestamp;
+        emit PickedWinner(winner);
+        (bool success, ) = winner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+    }
+
+    /**Getter functions */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+}
